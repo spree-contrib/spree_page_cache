b1e91c8b75230b530e5f3e8dc1ead1ea1be9f80b
Neeraj Singh <neerajdotname@gmail.com>
2011-04-22 10:45:36 -0400

    refactor js

--- public/javascripts/from_cookie.js
+++ public/javascripts/from_cookie.js
@@ -1,21 +1,29 @@
-function IsNumeric(input)
-{
-   return (input - 0) == input && input.length > 0;
-}
+var App = App || {};
 
+App.fromCookie =  {
 
-$(function() {
+	isNumeric: function(input) {
+		return (input - 0) == input && input.length > 0;
+	},
 
-  var	currentUserId = $.cookie('current_user_id'),
-		cartAmount = $.cookie('cart_amount');
+	udpateDom: function(){
+		var	currentUserId = $.cookie('current_user_id'),
+			cartAmount = $.cookie('cart_amount');
 
-	if(IsNumeric(currentUserId)){
-		$('#nav-bar li.logout').show();
-		$('#nav-bar li.account').show();
-	} else {
-		$('#nav-bar li.login').show();
+		if(isNumeric(currentUserId)){
+			$('#nav-bar li.logout').show();
+			$('#nav-bar li.account').show();
+		} else {
+			$('#nav-bar li.login').show();
+		}
+
+		$('#nav-bar li.cart-indicator').html(cartAmount).show();
 	}
+};
 
-	$('#nav-bar li.cart-indicator').html(cartAmount).show();
+
+$(function(){
+
+	App.fromCookie.updateDom();
 
 });
